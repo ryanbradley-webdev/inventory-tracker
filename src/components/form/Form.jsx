06f4f6db57c2841b261b83ae88dce8f9a2d9c1fc
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-16 12:24:02 -0500

    Update form component styling to be responsive. Add border and caret color on input focus.

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,23 +4,21 @@ import FormItem from "./FormItem"
 import PlusIcon from "../../assets/PlusIcon"
 import { useSearchParams } from "react-router-dom"
 import BackButton from "../BackButton"
+import styles from './form.module.css'
 
 export default function Form({ hideForm, isVisible, invoice }) {
     const [searchParams, setSearchParams] = useSearchParams()
 
     const [items, setItems] = useState(invoice?.items || [])
 
-    const styles = { 
+    const localStyles = { 
         modal: {
             position: 'absolute', 
             inset: '0',
             bottom: 'auto', 
-            background: 'var(--color-bg)', 
             transform: isVisible ? '' : 'translateX(-100%)',
             transition: 'transform 0.3s ease-in-out',
-            maxWidth: '616px',
-            paddingInline: '24px',
-            paddingBlock: '33px'
+            background: '#00000099'
         },
         button: {
             outline: 'transparent',
@@ -60,85 +58,99 @@ export default function Form({ hideForm, isVisible, invoice }) {
     }
 
     return (
-        <div style={styles.modal}>
-            <BackButton handleClick={closeForm} marginBottom='26px' />
-            {invoice ? (
-                    <h2>Edit <span>#</span>{invoice?.id}</h2>
-                ) : (
-                    <h2>New Invoice</h2>
-                )
-            }
-            <form action="" onSubmit={handleSubmit}>
-                <fieldset>
-                    <h4 style={styles.h4}>Bill From</h4>
-                    <label htmlFor="from-address">Street Address</label>
-                    <input type="text" name="from-address" id="from-address" />
-                    <div>
-                        <label htmlFor="from-city">City</label>
-                        <label htmlFor="from-post-code">Post Code</label>
-                        <input type="text" name="from-city" id="from-city" />
-                        <input type="text" name="from-post-code" id="from-post-code" />
-                    </div>
-                    <label htmlFor="from-country">Country</label>
-                    <input type="text" name="from-country" id="from-country" />
-                </fieldset>
-                <fieldset>
-                    <h4 style={styles.h4}>Bill To</h4>
-                    <label htmlFor="name">Client's Name</label>
-                    <input type="text" name="name" id="name" />
-                    <label htmlFor="email">Client's Email</label>
-                    <input type="email" name="email" id="email" />
-                    <label htmlFor="to-address">Street Address</label>
-                    <input type="text" name="to-address" id="to-address" />
-                    <div>
-                        <label htmlFor="to-city">City</label>
-                        <label htmlFor="to-post-code">Post Code</label>
-                        <input type="text" name="to-city" id="to-city" />
-                        <input type="text" name="to-post-code" id="to-post-code" />
-                    </div>
-                    <label htmlFor="to-country">Country</label>
-                    <input type="text" name="to-country" id="to-country" />
-                </fieldset>
-                <fieldset>
-                    <label htmlFor="date">Invoice Date</label>
-                    <input type="date" name="date" id="date" disabled={invoice} />
-                    <label htmlFor="term">Payment Terms</label>
-                    <select name="term" id="term">
-                        <option value=""></option>
-                    </select>
-                    <label htmlFor="description">Project Description</label>
-                    <input type="text" name="description" id="description" />
-                </fieldset>
-                <ul>
-                    {items.length > 0 && items.map(item => <FormItem item={item} removeItem={removeItem} updateItem={updateItem} />)}
-                </ul>
-                <Button>
-                    <PlusIcon />
-                    <span style={{ marginLeft: '0.5rem' }}>Add New Item</span>
-                </Button>
+        <div style={localStyles.modal}>
+            <div className={styles.wrapper}>
+                <BackButton handleClick={closeForm} marginBottom='26px' />
                 {invoice ? (
-                    <div>
-                        <Button>
-                            Discard
-                        </Button>
-                        <Button>
-                            Save As Draft
-                        </Button>
-                        <Button>
-                            Save & Send
-                        </Button>
-                    </div>
-                ) :  (
-                    <div>
-                        <Button>
-                            Cancel
-                        </Button>
-                        <Button>
-                            Save Changes
-                        </Button>
-                    </div>
-                )}
-            </form>
+                        <h2>Edit <span>#</span>{invoice?.id}</h2>
+                    ) : (
+                        <h2>New Invoice</h2>
+                    )
+                }
+                <form action="" onSubmit={handleSubmit}>
+                    <fieldset>
+                        <h4 style={localStyles.h4}>Bill From</h4>
+                        <label htmlFor="from-address">Street Address</label>
+                        <input type="text" name="from-address" id="from-address" />
+                        <div className={styles.address_info}>
+                            <label htmlFor="from-city">
+                                <span>City</span>
+                                <input type="text" name="from-city" id="from-city" />
+                            </label>
+                            <label htmlFor="from-post-code">
+                                <span>Post Code</span>
+                                <input type="text" name="from-post-code" id="from-post-code" />
+                            </label>
+                            <label htmlFor="from-country">
+                                <span>Country</span>
+                                <input type="text" name="from-country" id="from-country" />
+                            </label>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <h4 style={localStyles.h4}>Bill To</h4>
+                        <label htmlFor="name">Client's Name</label>
+                        <input type="text" name="name" id="name" />
+                        <label htmlFor="email">Client's Email</label>
+                        <input type="email" name="email" id="email" />
+                        <label htmlFor="to-address">Street Address</label>
+                        <input type="text" name="to-address" id="to-address" />
+                        <div className={styles.address_info}>
+                            <label htmlFor="to-city">
+                                <span>City</span>
+                                <input type="text" name="to-city" id="to-city" />
+                            </label>
+                            <label htmlFor="to-post-code">
+                                <span>Post Code</span>
+                                <input type="text" name="to-post-code" id="to-post-code" />
+                            </label>
+                            <label htmlFor="to-country">
+                                <span>Country</span>
+                                <input type="text" name="to-country" id="to-country" />
+                            </label>
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="date">Invoice Date</label>
+                        <input type="date" name="date" id="date" disabled={invoice} />
+                        <label htmlFor="term">Payment Terms</label>
+                        <select name="term" id="term">
+                            <option value=""></option>
+                        </select>
+                        <label htmlFor="description">Project Description</label>
+                        <input type="text" name="description" id="description" />
+                    </fieldset>
+                    <ul>
+                        {items.length > 0 && items.map(item => <FormItem item={item} removeItem={removeItem} updateItem={updateItem} />)}
+                    </ul>
+                    <Button>
+                        <PlusIcon />
+                        <span style={{ marginLeft: '0.5rem' }}>Add New Item</span>
+                    </Button>
+                    {invoice ? (
+                        <div>
+                            <Button>
+                                Discard
+                            </Button>
+                            <Button>
+                                Save As Draft
+                            </Button>
+                            <Button>
+                                Save & Send
+                            </Button>
+                        </div>
+                    ) :  (
+                        <div>
+                            <Button>
+                                Cancel
+                            </Button>
+                            <Button>
+                                Save Changes
+                            </Button>
+                        </div>
+                    )}
+                </form>
+            </div>
         </div>
     )
 }
