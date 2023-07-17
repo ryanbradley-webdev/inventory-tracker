92864e1de4e5e8d4d0b11d3fb050355326d7b230
Ryan Bradley <ryanbradley315@gmail.com>
2023-07-17 10:11:22 -0500

    Add change handler to update address state.

--- src/components/form/Address.jsx
+++ src/components/form/Address.jsx
@@ -1,22 +1,37 @@
+import { useRef } from 'react'
 import styles from './form.module.css'
 
-export default function Address({ street, city, postCode, country, toOrFrom }) {
+export default function Address({ street, city, postCode, country, toOrFrom, setAddress }) {
+    const streetRef = useRef(null)
+    const cityRef = useRef(null)
+    const postCodeRef = useRef(null)
+    const countryRef = useRef(null)
+
+    function handleChange() {
+        setAddress({
+            street: streetRef.current.value,
+            city: cityRef.current.value,
+            postCode: postCodeRef.current.value,
+            country: countryRef.current.value
+        })
+    }
+
     return (
         <>
             <label htmlFor={`${toOrFrom}-street`}>Street Address</label>
-            <input type="text" name={`${toOrFrom}-street`} id={`${toOrFrom}-street`} value={street} required />
+            <input type="text" name={`${toOrFrom}-street`} id={`${toOrFrom}-street`} value={street} onChange={handleChange} ref={streetRef} required />
             <div className={styles.address_info}>
                 <label htmlFor={`${toOrFrom}-city`}>
                     <span>City</span>
-                    <input type="text" name={`${toOrFrom}-city`} id={`${toOrFrom}-city`} value={city} required />
+                    <input type="text" name={`${toOrFrom}-city`} id={`${toOrFrom}-city`} value={city} onChange={handleChange} ref={cityRef} required />
                 </label>
                 <label htmlFor={`${toOrFrom}-post-code`}>
                     <span>Post Code</span>
-                    <input type="text" name={`${toOrFrom}-post-code`} id={`${toOrFrom}-post-code`} value={postCode} required />
+                    <input type="text" name={`${toOrFrom}-post-code`} id={`${toOrFrom}-post-code`} value={postCode} onChange={handleChange} ref={postCodeRef} required />
                 </label>
                 <label htmlFor={`${toOrFrom}-country`}>
                     <span>Country</span>
-                    <input type="text" name={`${toOrFrom}-country`} id={`${toOrFrom}-country`} value={country} required />
+                    <input type="text" name={`${toOrFrom}-country`} id={`${toOrFrom}-country`} value={country} onChange={handleChange} ref={countryRef} required />
                 </label>
             </div>
         </>
